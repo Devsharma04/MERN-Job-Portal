6d0c84beead9314a28f3f8d326d5cefbd7b581be
Dev Sharma <devsharma040102@gmail.com>
2025-03-18 14:26:46 +0530

    bug fix

--- Client/src/Components/UserAuths/Role.jsx
+++ Client/src/Components/UserAuths/Role.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import style from "./Role.module.css";
 import CursorAnimation from "../Animation/CursorAnimation";
